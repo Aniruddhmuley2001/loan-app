a8e1213d53df753e5d73db9d4b7aed7b592aa737
Aniket Ghosh <aniketghosh950@gmail.com>
2023-09-30 02:34:35 +0530

    Added the Ui of error page

--- loanapp-frontend/src/components/ErrorPage.js
+++ loanapp-frontend/src/components/ErrorPage.js
@@ -1,9 +1,21 @@
-import React from 'react'
-
-export default function ErrorPage() {
+import React from 'react';
+import { Container, Row, Col, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom'; // If you're using React Router for routing
+import '../css/ErrorPage.css'
+const ErrorPage = () => {
   return (
-    <div>
-        Check the URL once again :)
-    </div>
-  )
-}
+    <Container className="error-container">
+      <Row>
+        <Col md={8} className="mx-auto text-center">
+          <h1 className="display-4">404 - Page Not Found</h1>
+          <p className="lead">Sorry, the page you're looking for doesn't exist.</p>
+          <Link to="/">
+            <Button variant="primary">Go to Sign up page</Button>
+          </Link>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default ErrorPage;
